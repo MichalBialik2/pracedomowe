a89aace1e895ae0daaf284bec8278fc9d9c399da
MichalBialik2 <116037136+MichalBialik2@users.noreply.github.com>
2023-03-27 21:52:57 +0200

    Create rozkladanie.cs

---
+++ rozkladanie.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+                Console.Write("ta liczba rozkłada się na --> " + number + ", ");
+
+                for (int i = 2; i <= number; i++)
+                {
+                    while (number % i == 0)
+                    {
+                        Console.Write(i + " ");
+                        number /= i;
+                    }
+                }
+
+                Console.ReadLine();
+    }
+}
